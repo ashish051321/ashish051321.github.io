24c3a27ff49dc0bc99be3a10f43465bd8bf80b5a
Ashish Mishra <ashish.mishra@jetblue.com>
2017-12-16 17:41:09 +0530

    more interactive through mouseenter and mouseleave

--- myscripts.js
+++ myscripts.js
@@ -1,15 +1,31 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
 
 var btn = document.querySelector("#slideButton");
-var slider = document.querySelector("#slider");
+var slider = document.querySelector("#slider");//the entire sliding vertical menu
 
-btn.addEventListener("click",function(){
-    console.log("The value of left is " + slider.style.left);
-    if(slider.style.left == "-92px" )
-    slider.style.left = "10px"; 
-    else{
+// btn.addEventListener("click",function(){
+//     console.log("The value of left is " + slider.style.left);
+//     if(slider.style.left == "-92px" )
+//     slider.style.left = "10px"; 
+//     else{
+//     slider.style.left = "-92px"; 
+//     }
+// });
+
+var allevents = "mouseenter";
+e= document.querySelector("#slider");
+addListenerMulti(e,allevents,fn);
+
+function addListenerMulti(el, s, fn) {
+    s.split(' ').forEach(e => el.addEventListener(e, fn, false));
+  }
+
+function fn(){
+    slider.style.left = "0px"; 
+}//function fn ends
+
+e.addEventListener("mouseleave",function(){
     slider.style.left = "-92px"; 
-    }
 });
 
-});
+});//DOMContentLoaded method
