e9ca790f6b253d02137b05e8310652c6c0992a13
Ashish Mishra <ashish.mishra@jetblue.com>
2017-12-16 16:44:19 +0530

    added base64 burger and did the animation of vertical menu slider

--- myscripts.js
+++ myscripts.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
 
-var btn = document.querySelector("#toggleSideBar");
-var slider = document.querySelector("#mainVerticalMenu");
+var btn = document.querySelector("#slideButton");
+var slider = document.querySelector("#slider");
 
 btn.addEventListener("click",function(){
     console.log("The value of left is " + slider.style.left);
-    if(slider.style.left == "-500px" )
+    if(slider.style.left == "-92px" )
     slider.style.left = "10px"; 
     else{
-    slider.style.left = "-500px"; 
+    slider.style.left = "-92px"; 
     }
 });
 
