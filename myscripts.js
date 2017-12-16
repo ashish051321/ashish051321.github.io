73ad69ff912b6f15ff476df70be9876fb9830f47
Ashish Mishra <ashish.mishra@jetblue.com>
2017-12-16 18:15:28 +0530

    added a single strip for user action. vetical menu interactivity improved

--- myscripts.js
+++ myscripts.js
@@ -6,7 +6,7 @@ var slider = document.querySelector("#slider");//the entire sliding vertical men
 // btn.addEventListener("click",function(){
 //     console.log("The value of left is " + slider.style.left);
 //     if(slider.style.left == "-92px" )
-//     slider.style.left = "10px"; 
+//     slider.style.left = "10px" ; 
 //     else{
 //     slider.style.left = "-92px"; 
 //     }
@@ -20,12 +20,15 @@ function addListenerMulti(el, s, fn) {
     s.split(' ').forEach(e => el.addEventListener(e, fn, false));
   }
 
+  
 function fn(){
     slider.style.left = "0px"; 
+    document.querySelector("#shadowForHover").style.display = "none";
 }//function fn ends
 
 e.addEventListener("mouseleave",function(){
-    slider.style.left = "-92px"; 
+    slider.style.left = "-100px"; 
+    document.querySelector("#shadowForHover").style.display = "block";
 });
 
 });//DOMContentLoaded method
